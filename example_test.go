76c27f6852affae724a5ba22cbd60eba62f4ff2a
jkrecek <killerfrca@gmail.com>
2016-04-06 14:35:15 +0200

    Add test
    Signed-off-by: jkrecek <killerfrca@gmail.com>

--- example_test.go
+++ example_test.go
@@ -8,6 +8,7 @@ type testConfig struct {
 	ExampleBool   bool             `json:"example_bool" default:"true"`
 	ExampleString string           `json:"example_str" default:"empty"`
 	ExampleInt    int              `json:"example_int" default:"50"`
+	ExampleInt20  int              `json:"example_int_20" default:"999"`
 	Mysql         *MysqlConnection `json:"mysql" testable:"true"`
 	MysqlStruct   MysqlConnection  `json:"mysql_struct" tastable:"true"`
 	MysqlNoTest   MysqlConnection  `json:"mysql_no_test" testable:"false"`
@@ -16,7 +17,8 @@ type testConfig struct {
 // TODO must somehow use mock mysql connection
 var exampleConfig = []byte(`
 {
-  "Mysql"             : {
+  "example_int_20"    : 20,
+  "mysql"             : {
     "Address"   : "localhost:3306",
     "User"      : "root",
     "Pass"      : "usbw",
@@ -39,6 +41,10 @@ func TestConfig(t *testing.T) {
 
 	if configObj.ExampleInt != 50 {
 		t.Fatal("ExampleInt has unexpected value")
+	}
+
+	if configObj.ExampleInt20 != 20 {
+		t.Fatal("ExampleInt20 has unexpected value")
 	}
 
 }
